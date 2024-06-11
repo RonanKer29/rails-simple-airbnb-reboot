3b19febf0beeabad0ca8897962f1e64a29e61650
RonanKer29 <kervella.ronan@hotmail.com>
2024-06-11 14:00:10 +0200

    new flat form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,8 +3,26 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
-  def show
+ def show
     @flat = Flat.find(params[:id])
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path, notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image_url)
+  end
 end
